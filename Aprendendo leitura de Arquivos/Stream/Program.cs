1e64e8cb5698f6ce661096eef6989b22fc9ff4c9
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-10-03 07:14:12 -0300

    recuperando exercicios de StreamRead

--- Aprendendo leitura de Arquivos/Stream/Program.cs
+++ Aprendendo leitura de Arquivos/Stream/Program.cs
@@ -42,7 +42,72 @@ Exemplo1.Close();
 //vamos para os exercicios
 
 //1) nesse aqui, devemos pedir um caminho de arquivo e uma palavra ou caracter
-// devemos imprimir somente as linhas que eomeçam com o que o usuario informou
+// devemos imprimir somente as linhas que começam com o que o usuario informou
 // após isso mostrar quantas linhas começam com isso que o usuario pediu
 
-Console.WriteLine("digite o caminho do arquivo"
+Console.WriteLine("digite o caminho do arquivo");
+string path = Console.ReadLine();
+Console.WriteLine("por favor, digite uma palavra para trazer as linhas que começam com ela");
+string search = Console.ReadLine();
+
+System.IO.StreamReader archive = new System.IO.StreamReader(path);
+int contador = 0;
+
+while (!archive.EndOfStream)
+{
+    string linha = archive.ReadLine();
+    if (linha.ToLower().StartsWith(search.ToLower()))
+    {
+        contador++;
+        Console.WriteLine(linha);
+    }
+}
+Console.WriteLine($"a palavra pesquisada apareceu {contador} vezes no inicio de uma linha");
+
+
+////3)nesse exercicio, devo pedir o caminho, e uma palavra.
+//// para qualquer incidencia dessa palavra, a linha inteira será escrita
+
+//Console.WriteLine("digite o caminho do arquivo");
+//string caminho3  = Console.ReadLine();
+//Console.WriteLine("digite uma palavra para filtrar quais linhas serão impressas");
+//string Search =  Console.ReadLine();
+
+//System.IO.StreamReader leitura = new System.IO.StreamReader(caminho3);
+
+//while (!leitura.EndOfStream) 
+//{
+//    string linha = leitura.ReadLine();
+//    if (linha.ToLower().Contains(Search.ToLower()))
+//    {
+//        Console.WriteLine(linha);
+//    }
+//}
+//leitura.Close();
+
+//4) nesse exercicio, devemos imprimir somente a maior linha que tiver no arquivo
+// aqui vamos ver o tamanho de cada uma e somente quando a proxima for maior, vamos salvar esse valor
+// ao final, imprimimos a maior
+
+using
+
+System.IO.StreamReader arquivo = new StreamReader("texto.txt");
+
+int tamanhoLinhaAtual;
+int MaiorAnterior = 0;
+int linhaMaior = 1;
+string maiorLinha = "";
+
+while (!arquivo.EndOfStream)
+{
+    string linha = arquivo.ReadLine();
+    tamanhoLinhaAtual = linha.Length;
+
+    if (tamanhoLinhaAtual > MaiorAnterior)
+    {
+        linhaMaior += 1;
+        MaiorAnterior = tamanhoLinhaAtual;
+        maiorLinha = linha;
+    }
+}
+Console.WriteLine(maiorLinha);
