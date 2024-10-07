b9e66a48a315ea56c9fe9de7717b7b0159984d87
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-10-07 15:23:54 -0300

    aprendendo nuget Epplus Excel

---
+++ Aprendendo NuGet/Aprendendo NuGet/Program.cs
@@ -0,0 +1,81 @@
+﻿
+// Para mexer com documentos Excel, tanto criar, quanto escrever, etc. 
+// Podemos usar o EPPlus do Nuget, que contem algumas funções exclusivas para isso
+
+// Sua princpal classe que vai permitir a maior parte das alterações é o "ExcelPackage",
+// que é uma classe não estática(instanciavel), ou seja, necessita ser iniciada e ter seu valor passado para uma variavel
+// Excel Package recebe um valor principal, sendo um arquivo já existente para trabalhar em cima, ou uma MemoryStream,
+// que serve para ir criando o excel na memoria Ram e somente após terminar, nós o criamos como arquivo.
+
+// primeiramente, vou explicar um pouco sobre a MemoryStream.
+// Como dito antes, ela permite criar algo na memoria Ram, construindo byte por byte,
+// ao final da edição do "arquivo", nós podemos dar um fim a ele, desde criar realmente um arquivo no computador, ou
+// importar esses dados em bytes para outro local, e lá esses dados serão analisados e reconstituidos em um arquivo de verdade.
+// Quando criamos uma variavel "MemoryStream", é como se criassemos uma "caixa" temporaria na memoria ram,
+// e dentro dessa caixa, nós colocamos tudo o que queremos usar para contruir algo. Quando terminamos, salvamos essa caixa,
+// e então damos um uso para ela.
+
+// Agora sim, irei explicar sobre ExcelPackage.
+// ExcelPackage pode receber tanto o caminho de um arquivo existente, quanto a variavel MemoryStream(criação na ram).
+// Ela é uma classe que permite a criação de uma variavel que contem informações necessárias para criação e edição de planilhas.
+// essa variavel possui diversas propriedades e métodos, algumas propriedade são:
+// 1 - workbook(pense nele como se fosse todo o arquivo do excel... o como se fosse o "arquivo" inteiro)
+// 2 - worksheets(são as abas do excel, as varias planilhas que criamos dentro de um arquivo)
+// 3 - cells(as celulas e seus conteudos internos)
+// Cada propriedade aprofunda mais sobre algo, e cada propriedade possui seus proprios métodos caso queira usar
+
+// para entendermos um pouco da ligação entre o MemoryStream e o ExcelPackage, pense no stream como uma caixa,
+// pense nesse exemplo: "var caixaExcel = ExcelPackage("caixaStream")"
+// definimos que dentro de caixaExcel, só serão guardadas informações sobre Excel, e quando queremos guardar algo, chamamos ela.
+// Ao final, quando acabamos e já colocamos tudo que queriamos na caixa, salvamos ela e pedimos para "outra pessoa" montar.
+
+// uma coisa MUITO importante para quando criamos um excel do zero e queremos salvar em arquivo...
+// Quando estamos "guardando as coisas na caixa", é como se estivessemos escrevendo com uma caneta,
+// conforme vamos escrevendo, a caneta vai indo para frente e assim vai até chegar no final e acabar de escrever,
+// mas para ler o que escrevemos, precisamos voltar a caneta no inicio e ir passando letra por letra para ajudar na leitura, certo?
+// Aqui é a mesma coisa, enquanto criamos o excel, a posição da variavel MemoryStream vai indo para frente, byte por byte,
+// e quando terminamos de criar, a posição dela está nos ultimos bytes.
+// Só que quando criamos um arquivo, usamos File.ReadAllBytes() que lê os bytes da posição Stream em frente,
+// após isso ele cria o arquivo, mas ele não pode ler nada se a posição Stream estiver nos ultimos bytes.
+// Para resolver isso, nós fazemos "variavelStream.Position = 0"
+// Assim nós resetamos a posição e após isso pedimos para criar o arquivo
+
+
+
+
+// Quando queremos criar uma aba, depois de .workbook, usamos o .WorkSheet.add("nome da aba"),
+// armazenamos esse valor em uma variavel e cada variavel desse tipo, será uma nova aba de uma arquivo excel
+
+//ExcelWorksheet aba1 = package.Workbook.Worksheets.Add("minha planilha");
+
+
+// E para editar as células? como faço para dizer qual eu quero mexer e o que eu quero escrecer?
+// podemos acessar .cells[] pela variavel da aba que criamos
+// .cells[] diz em qual célula nós vamos escrever, dentro dela podemos colocar ["B1"], ["A3"], ["Z7"], ["Y14"] por exemplo.
+// E para escrever, nós usamos .value = "". 
+
+
+using OfficeOpenXml;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // If you use EPPlus in a noncommercial context
+            // according to the Polyform Noncommercial license:
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var stream = new MemoryStream();
+            var package = new ExcelPackage(stream);
+
+            ExcelWorksheet aba1 = package.Workbook.Worksheets.Add("minha planilha");
+            aba1.Cells["A1"].Value = "1";
+
+            stream.Position = 0;
+            File.WriteAllBytes("excel.xlsx", stream.ToArray());
+
+        }
+    }
+}
---
+++ Aprendendo NuGet/NuGet/Program.cs
@@ -0,0 +1,20 @@
+﻿// Nuget é uma plataforma online onde podemos onstalar componentes prontos com diversas funções.
+// Esses componentes vão desde contas simples até fazer verificações de API
+// todos esses são criados pela comunidade, onde podemos pegar o link da instalação e baixar no nosso programa
+
+// Digamos que você queira criar uma planilha do Excel, no Nuget encontramos componentes de criação de planilhas
+// onde podemos usar livremente para criar, editar, etc.
+
+
+// Para baixar o componente, basta copiar o link de instalação dá pagina
+
+// Ao entrar na página de um componente, podemos ver um comando de intalação com esse:
+// Install-Package EPPlus -Version 7.4.0 que devemos copiar,
+// e na aba ferramentas(tools) no visual studio/Gerenciador de pacotes nuget/console, podemos colar
+// e simples assim, está instalado
+
+// E para saber como usar? Normalmente, cada componente, no site do Nuget, terá um link do site de quem criou,
+// lá deve ter toda a documentação explicando o que é, para que serve, como usar cada método do componente,
+// basicamente um manual de instruçõe. E sempre leia tudo, para saber tudo que é possivel fazer com ele,
+// pois se por acaso está enfrentando um problema e se lembra que o componente pode te ajudar, você pode voltar e aprender,
+// mas se não ler, vai bater a cabeça pensando em outras soluções.
