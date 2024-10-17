c8645d990f1c2d9bde7b8d5f9a966dd57bfac9f9
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-10-17 05:50:54 -0300

    começando orientação a Apis

---
+++ Aprendendo a manipular API/Aprendendo a manipular API/Program.cs
@@ -0,0 +1,29 @@
+﻿// Vamos falar um pouco sobre APIs hoje, e suas utilidade gerais
+
+// API significa "Application Programming Interface". É um conjunto de regras
+// que permite a comunicação entre dois sistemas diferentes. Pense em uma API
+// como uma ponte que conecta seu aplicativo a outros serviços, permitindo que
+// você use dados ou funções que eles fornecem.
+
+// Muitas grandes empresas e serviços, como Google, Twitter, Facebook e TMDb
+// (The Movie Database), oferecem suas próprias APIs. Isso permite que você
+// possa acessar dados como informações de filmes, clima, e muito mais.
+// Essas empresas compartilham seus dados e funcionalidades com outros
+// desenvolvedores por meio de APIs, que funcionam como canais de comunicação.
+
+//Para usar uma API, você precisa seguir as regras e instruções fornecidas
+// pela "documentação" da API. Que geralmente é encontrada onde a Api é fornecida pelo criador.
+// A documentação ensina como fazer requisições
+// (como o garçom que leva seu pedido à cozinha) e quais dados você pode
+// acessar ou enviar. Sem a documentação, seria como tentar falar uma língua
+// desconhecida sem dicionário.
+
+// APIs costumam enviar e receber dados em formatos padronizados como JSON
+// (JavaScript Object Notation). Por exemplo, quando você faz uma requisição
+// para uma API que fornece dados sobre usuários, a resposta pode vir assim:
+
+// {
+//     "name": "John",
+//     "age": 30,
+//     "city": "New York"
+// }
