d84cf9844c203c8e3354c6a2f4a6c922412431f9
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-10-28 07:34:17 -0300

    chamada de Api

---
+++ Aprendendo a manipular API/Desafio 05/Api.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_05
+{
+    internal class Api
+    {
+        public async Task<Infos> Request()
+        {
+            var client = new RestClient("https://api.coinbase.com/v2/currencies");
+            var request = new RestRequest("", Method.Get);
+
+            try
+            {
+                RestResponse response = await client.ExecuteAsync(request);
+
+                if (response.IsSuccessful)
+                {
+                    Infos infos = JsonConvert.DeserializeObject<Infos>(response.Content);
+                    return infos;
+                }
+                else
+                {
+                    Console.WriteLine($"Erro: {response.StatusCode} - {response.ErrorMessage}");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exceção: {ex.Message}");
+                return null; 
+            }
+        }
+    }
+}
--- Aprendendo a manipular API/Desafio 05/Program.cs
+++ Aprendendo a manipular API/Desafio 05/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Nesse desafio, devo utilizar a Api da CoinBase(https://api.coinbase.com/v2/currencies) para:
+// exibir os dados de forma organizada e exibir o valor minimo em 6 casas decimais
+
+using Desafio_05;
+
+Api api = new Api();
+
+Infos dados = await api.Request();
+
+foreach (var info in dados.data)
+{
+    Console.WriteLine(info.id);
+    Console.WriteLine(info.name);
+    Console.WriteLine(info.min_size.ToString("N6"));
+}
+
