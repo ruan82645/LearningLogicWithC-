33a68a13d66c7a26c55d01bab2ac24440233da62
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-10-28 03:40:32 -0300

    chamada da api

---
+++ Aprendendo a manipular API/Desafio 03/Chamada de Api.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_03
+{
+    internal class Chamada_de_Api
+    {
+        public async Task<List<Infos>> filmes(int pagina)
+        {
+
+            RestClient client = new RestClient("https://api.themoviedb.org/3/");
+            client.AddDefaultHeader("api_Key", "c634b4775a037a56f5dd2baf3951c4e8");
+            var request = new RestRequest($"movie/popular?api_key=c634b4775a037a56f5dd2baf3951c4e8&page={pagina}", Method.Get);
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                var popularMovies = JsonConvert.DeserializeObject<List<Infos>>(response.Content);
+                return popularMovies;
+            }
+            else
+            {
+                throw new Exception("Erro ao obter dados da API: " + response.ErrorMessage);
+            }
+        }
+    }
+
+}
+
--- Aprendendo a manipular API/Desafio 03/obj/Debug/net8.0/Desafio 03.AssemblyInfo.cs
+++ Aprendendo a manipular API/Desafio 03/obj/Debug/net8.0/Desafio 03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafio 03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f4cab1fcdd655b2ff5ec3ccf17a544b93f2614")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3dd7be4beab0247b0b0fb89846be712ced38564")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafio 03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafio 03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
