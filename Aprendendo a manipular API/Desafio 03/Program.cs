398a87444ac25df795906a4eb972d83cce7d7d04
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-10-28 04:04:06 -0300

    estrutura base pronta

--- Aprendendo a manipular API/Desafio 03/Program.cs
+++ Aprendendo a manipular API/Desafio 03/Program.cs
@@ -2,5 +2,42 @@
 // O objetivo é exibir uma "página" de filmes e perguntar se o usuario deseja continuar vendo
 // se disser sim, deve ser exibida outra página
 
-int i = 1;
+using Desafio_03;
+
+int pagina = 1;
+string confirmacao = "";
+
+while (confirmacao != "não")
+{
+    Api api = new Api();
+    Formatar formatar = new Formatar();
+
+    List<Infos> Movies = await api.filmes(pagina);
+
+    foreach (Infos Movie in Movies)
+    {
+        formatar.Exibir(Movie);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("ir para a proxima página? (sim ou não?)");
+    confirmacao = Console.ReadLine();
+
+    if(confirmacao.ToLower() == "sim" || confirmacao.ToLower() == "não")
+    {
+        if (confirmacao == "sim")
+        {
+            pagina++;
+        }
+        if (confirmacao == "não")
+        {
+            break;
+        }
+    }
+    else
+    {
+
+    }
+    
+}
 
