73f3922be8638dc7ea9744747bec1634956cd07a
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-10-29 07:12:22 -0300

    entendendo diagramas e seleçoes

---
+++ Aprendendo SQL Server/Aprendendo SQL Server/Program.cs
@@ -0,0 +1,20 @@
+﻿// Nesse módulo, vamos entender um pouco mais sobre o SQL Server. Para que serve, como usar...
+
+// SQL Server é onde criamos e editamos bancos de dados para serem utilizados em nossa aplicação ou site,
+// lá pode conter todas as informações de usuários que acessam seu site, que fazem um pedido, compra.
+// Lá registramos tudo o que acontece por trás da tela, é o lugar onde se armazena tudo,
+// por exemplo: podemos ter um banco de dados em um site de compra, um "e-commerce". Quando o cliente-
+// digita seu usuario e senha, vamos até o banco de dados onde estavam armazenadas essas informações,
+// conferimos o usuario e se a senha realmente bate com aquilo e então liberamos o acesso
+
+
+// Uma coisa importante, um banco de dados no sql server é muito parecido com uma planilha do excel
+// Pense em uma planilha de um veterinário, lá tem alguns tipos de dados, como:
+// nome do dono, email do dono, endereço do dono, profissão, etc...
+// também há nome do pet, idade do pet, doenças anteriores, raça, e etc...
+// Agora imagine que esa planilha está organizada da seguinte forma:
+
+// | nomeDono |    email      | endereço |  profissão  | nomePet | idadePet | doençasAnteriores  |   raça   |
+//---------------------------------------------------------------------------------------------
+// | Larissa  | lri@gmail.com |  QE 40   | Professora  |  Oddie  | 12 anos  | problema de coluna | salsicha |
+
