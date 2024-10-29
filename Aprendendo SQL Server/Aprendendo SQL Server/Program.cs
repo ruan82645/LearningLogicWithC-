77000469fcbd32a04429871e2d3e1b24116ad26d
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-10-29 07:46:38 -0300

    atualizando commit anterior

--- Aprendendo SQL Server/Aprendendo SQL Server/Program.cs
+++ Aprendendo SQL Server/Aprendendo SQL Server/Program.cs
@@ -31,5 +31,32 @@
 // Agora quando adicionarmos um pet novo, não precisamos repetir todas as informações do dono
 // podemos apenas colocar o id do dono ali, então vamos saber que aquele pet pertence a tal dono
 
-// Dessa forma, o cliente seria cadastrado uma unica vez, sem repetições bobas, vou mostrar em exemplo
+// Dessa forma, o cliente seria cadastrado uma unica vez, sem repetições bobas, vou mostrar em exemplo:
 
+// Cadastro de Donos:
+// | DonoId  | nomeDono |    email       | endereço |  profissão  |
+//-----------------------------------------------------------------
+// |   032   | Larissa  | lri@gmail.com  | QE 40    | Professora  |
+// |   057   | Ruan     | ruan@gmail.com | QE 38    | Dev Junior  |
+// |   091   | Leomar   | leo@gmail.com  | QE 21    | Bombeiro    |
+
+// temos 3 clientes cadastrados, agora vamos cadastrar os pets, onde cada dono terá 2 pets diferentes
+
+// Cadastro de Pets:
+// | DonoId  | nomePet  | idadePet | doençasAnteriores  |   raça    |
+// -----------------------------------------------------------
+// |   032   | Oddie    | 12 anos  | problema de coluna | salsicha  |
+// |   032   | Pipoca   | 04 anos  | trauma de chuva    | vira-lata |
+// |   057   | Ravena   | 01 ano   | Nenhum             | salsicha  | 
+// |   057   | Sasha    | 05 anos  | Febre              | Golden    |
+// |   091   | Jackie   | 03 anos  | pneumonia          | Huskie    |
+// |   091   | Romeu    | 07 anos  | Nenhum             | pastor    |
+
+// Note que eu não preciso mais ficar colocando todas as informações do dono, eu apenas ligo os dois pelo id.
+// é assim que o banco de dados funciona para diversos tópicos, onde podem haver diversos ids para outras coisas.
+
+// Agora, imagine uma loja de bicicletas, nela nós temos o cliente, o pedido, produtos, lojas fisicas, estoques, etc.
+// cada produto(bicicleta) tem um id, possui uma marca(um id diferente), categoria(outro id).
+// cada estoque tem um id, cada loja tem ids diferentes que podem ser determinada cidade, tamanho, etc
+
+// para cada coisa, podemos ter varios ids, que nos ajudam a conectar tópicos diferentes de uma forma mais fácil e organizada
