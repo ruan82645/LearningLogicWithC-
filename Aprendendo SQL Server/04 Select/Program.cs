27f75baf9c5b75e03d97c5737f23e3abf4bcdd53
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-10-30 05:54:07 -0300

    como funciona o select

--- Aprendendo SQL Server/04 Select/Program.cs
+++ Aprendendo SQL Server/04 Select/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Nesse Módulo, vamos entender melhor o que é, quais funções, modos de uso e por existe o Select.
+
+// Quando temos um banco de dados com muitas informações, com certeza teremos varias tabelas, cada uma organizada
+// porém as vezes precisamos fazer filtragens dentro de uma unica tabela ou mais.
+// por exemplo: pegar somente os nomes de quem tem Hotmail na tabela de clientes
+// ou mesclar informações especificas de varias tabelas diferentes para vizualizar os dados de melhor forma
+
+// Quando estmos olhando uma tabela, vemos como uma página de excel, aparecem as colunas e dados que escrevemos lá
+// mas assim como no excel, as vezes de 10 tipos de informação, queremos ver apenas 3, ou ver dados de pessoas especificas.
+// Imagine que em uma aba, temos informações de pedidos, já em outra de clientes. E eu quero saber quais pessoas-
+// compraram uma determinada bicicleta, para isso, eu pegaria as informações de pedido e juntaria com os clientes
+// assim eu formaria um "aba nova" com os dados que EU quero analisar ou utilizar
+
+// Mas diferente do Excel, não fazemos isso apenas com o mouse, devemos fazer por uma linha de código.
+// Ela é extremamente simples e é escrita da mesma forma que escrevemos na vida real.
+// Esse é o Select, um comando que seleciona informações que você deseja vizualisar e exibe para você na tabela abaixo
+
+
