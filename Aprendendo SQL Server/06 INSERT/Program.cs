5225d7f8e302624059876c022cc95a82f747514c
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-11-04 12:06:55 -0300

    exemplos de insert e como usar

--- Aprendendo SQL Server/06 INSERT/Program.cs
+++ Aprendendo SQL Server/06 INSERT/Program.cs
@@ -9,5 +9,46 @@ então se na tabela que deseja incluir tiver essa opção ativa(vai ter o icona 
 será automaticamente adicionado um novo id, sem depender de você! Então mesmo que queiramos escrever o id ao colocar manualmente,
 nós não possuimos permissão, vai dar erro. Todo o restante é preenchivel, menos o ID
 
+Certo, vamos para como funciona o comando então:
+
+primeiro selecionamos a qual tabela vamos inserir os dados, dessa forma:
+
+(INSERT INTO "TABELA")
+
+e então abrimos chaves. E dentro dessas chaves dizemos quais campos vamos usar para inserir os valores.
+Vale lembrar que nem sempre todos campos são obrigátorios, mas devemos verificar se são, pois se for obrigatorio e não o citarmos-
+haverá erro!
+
+Insert into CLIENTES
+(
+nome, 
+sobrenome,
+idade,
+endereco 
+)
+
+após dizemos quais os campos serão preenchidos, colocamos "Values" e abrimos novamente os parenteses-
+e dentro deles preenchemos os dados...
+Sempre na mesma ordem na qual declaramos os campos, então se o primeiro campo é nome, o primeiro valor será nome
+
+Insert into CLIENTES
+
+(
+nome, 
+sobrenome,
+idade,
+endereco 
+)
+
+Values
+(
+'ruan' ,
+'alves' ,
+20 ,
+''
+)
+
+note que deixei o endereço em branco usando string vazia. Caso esse campo fosse obrigatório e eu não soubesse o que colocar, -
+eu poderia colocar string vazia, afinal, são dados diferentes
 
 */
